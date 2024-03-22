c5b5ef1e10b88456f8b15db7e1c075aeb4dfc935
slrspa14 <kw32482002@gmail.com>
2024-03-22 19:21:23 +0900

    server, client

---
+++ AI_project_client/AI_project_client/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Windows;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+// OpenCV 사용을 위한 using
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+// Timer 사용을 위한 using
+using System.Windows.Threading;
+using System.Text;
+
+namespace AI_project_client
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        VideoCapture cam;
+        Mat frame;
+        DispatcherTimer timer;
+        bool is_initCam, is_initTimer;
+        public NetworkStream stream;//통신스트림
+        public TcpClient client = new TcpClient();//연결소켓
+        byte[] message = new byte[10];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_loaded(object sender, RoutedEventArgs e)
+        {
+            // 카메라, 타이머(0.01ms 간격) 초기화
+            is_initCam = Init_camera();
+            is_initTimer = Init_Timer(0.01);
+
+            // 초기화 완료면 타이머 실행
+            if (is_initTimer && is_initCam) timer.Start();
+        }
+
+        private bool Init_Timer(double interval_ms)
+        {
+            try
+            {
+                timer = new DispatcherTimer();
+
+                timer.Interval = TimeSpan.FromMilliseconds(interval_ms);
+                timer.Tick += new EventHandler(Timer_tick);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool Init_camera()
+        {
+            try
+            {
+                // 0번 카메라로 VideoCapture 생성 (카메라가 없으면 안됨)
+                cam = new VideoCapture(0);
+                if(!cam.IsOpened())
+                {
+                    MessageBox.Show("웹캠을 열 수 없습니다.");
+                    Environment.Exit(0);//강제종료
+                }
+                cam.FrameHeight = (int)Cam.Height;
+                cam.FrameWidth = (int)Cam.Width;
+
+                // 카메라 영상을 담을 Mat 변수 생성
+                frame = new Mat();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Server_connect_Click(object sender, RoutedEventArgs e)
+        {
+            //연결소켓
+            //client = new TcpClient();
+            try
+            {
+                client.Connect("10.10.20.113", 9196);
+                if (client.Connected)
+                {
+                    MessageBox.Show("접속 성공");
+                    string test = "1/클라접속";
+                    stream = client.GetStream();
+                    message = Encoding.Default.GetBytes(test);
+                    stream.Write(message, 0, message.Length);
+                    File_send();
+                    Receive_result();
+                }
+                else
+                    MessageBox.Show("접속 실패");
+            }
+            catch(Exception c)
+            {
+                MessageBox.Show("오류");
+                Console.WriteLine(c);
+            }
+        }
+
+        private void Timer_tick(object sender, EventArgs e)
+        {
+            // 0번 장비로 생성된 VideoCapture 객체에서 frame을 읽어옴
+            cam.Read(frame);
+            // 읽어온 Mat 데이터를 Bitmap 데이터로 변경 후 컨트롤에 그려줌
+            Cam.Source = WriteableBitmapConverter.ToWriteableBitmap(frame);
+            
+        }
+
+        //실시간 캡처본
+        private async void File_send()//되려나
+        {
+            await Task.Run(async() =>
+               {
+                   while (true)
+                   {
+                       await Task.Delay(1000);
+                       byte[] file_ready = new byte[4];
+                       file_ready = Encoding.Default.GetBytes("1/2");//보내서 서버에서 파일받을 준비하라고
+                       stream.Write(file_ready, 0, file_ready.Length);
+                       Mat capture_image = new Mat();
+                       cam.Read(capture_image);
+
+                       byte[] file_length = new byte[4];
+                       file_length = BitConverter.GetBytes(capture_image.ToBytes().Length);
+                       stream.Write(file_length, 0, file_length.Length);//파일크기 보내주고
+
+                       byte[] file_data = new byte[capture_image.ToBytes().Length];//파일크기 배열 생성
+                       file_data = capture_image.ToBytes();
+                       stream.Write(file_data, 0, file_data.Length);
+                       
+                       Array.Clear(file_ready, 0, file_ready.Length);
+                       Array.Clear(file_length, 0, file_length.Length);
+                       Array.Clear(file_data, 0, file_data.Length);
+                   }
+               });
+        }
+        private async void Receive_result()//되려나
+        {
+            await Task.Run(() =>
+                {
+                    byte[] recv_result = new byte[256];
+                    stream.Read(recv_result, 0, recv_result.Length);
+                    //결과 라벨에 띄우기
+                    string result = Encoding.Default.GetString(recv_result);
+                    //result_label.Content = result;//라벨
+                    MessageBox.Show(result);
+                });
+            
+        }
+    }
+
+}
---
+++ AI_project_server/AI_project_server/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows;
+using System.Net.Sockets;
+using System.Net;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AI_project_server
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string bindIP = "10.10.20.113";
+        private const int bindPort = 9196;
+        private TcpListener server = null;
+        private IPEndPoint localAddress;
+        TcpClient client;
+        NetworkStream stream;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_btn_Click(object sender, RoutedEventArgs e)
+        {
+            //서버열기
+            //listen
+            try
+            {
+                localAddress = new IPEndPoint(IPAddress.Parse(bindIP), bindPort);
+                server = new TcpListener(localAddress);
+                server.Start();
+                MessageBox.Show("Server Open");
+                Thread t1 = new Thread(new ThreadStart(Connect));
+                t1.Start();
+                //Connect_test();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                server.Stop();
+            }
+        }
+        private void Connect()
+        {
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                stream = client.GetStream();
+                int length;
+                byte[] data = new byte[128];
+                while ((length = stream.Read(data, 0, data.Length)) != 0)//파일크기 받고 파일받기
+                {
+                    //연결 확인용
+                    string test = Encoding.Default.GetString(data);
+                    MessageBox.Show(test);
+                    byte[] test_msg = new byte[128];
+                    test_msg = Encoding.Default.GetBytes("가나요");//잘 옴
+                    stream.Write(test_msg, 0, test_msg.Length);
+                }
+            }
+        }
+        //private async void Connect_test()
+        //{
+        //    await Task.Run(async () =>
+        //    {
+        //        while (true)
+        //        {
+        //            await Task.Delay(1000);
+        //            client = server.AcceptTcpClient();
+        //            stream = client.GetStream();
+        //            int length;
+        //            byte[] data = new byte[128];
+        //            while ((length = stream.Read(data, 0, data.Length)) != 0)//파일크기 받고 파일받기//클라가 연결 끊었을 때
+        //            {
+        //                //연결 확인용
+        //                string test = Encoding.Default.GetString(data);
+        //                MessageBox.Show(test);
+        //                byte[] test_msg = new byte[128];
+        //                test_msg = Encoding.Default.GetBytes("가나요");//잘 옴
+        //                stream.Write(test_msg, 0, test_msg.Length);
+        //            }
+        //        }
+        //    });
+            
+        //}
+    }
+}
